82ab56a2988a2f01174d1451833ffe1cdd112756
Apiaceae <wangls76@gmail.com>
2013-02-17 22:31:47 +0800

    Initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '93b028b0bda3c8ffb0cede36a03740bcbdd4563c7d9bc1df5fef40cdf3d0571c5df49968954b50027ebb5f67b2b3ca6d4e753954c59d3f05a8ba8e2ec7d8aa48'
